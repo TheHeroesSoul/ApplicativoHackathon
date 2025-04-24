54ea079d19de0cb1d036f3096ac0bf00dfe487f8
Lorenzo Lista <99364774+TheHeroesSoul@users.noreply.github.com>
2025-04-24 20:46:52 +0200

    Aggiunta del Codice

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,29 +1,65 @@
 package model;
 
-/**
- * The type Utente.
- */
 public class Utente {
-    private final String login;
-    private String password;
+    protected int id;
+    protected String username;
+    protected String email;
+    protected String nome;
+    protected String cognome;
+    protected boolean isRegistrato;
 
-    /**
-     * Instantiates a new Utente.
-     *
-     * @param login    the login
-     * @param password the password
-     */
-    public Utente(String login, String password) {
-        this.login = login;
-        this.password = password;
+    public Utente(int id, String username,String email, String nome, String cognome) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.isRegistrato = false;
     }
 
-    /**
-     * Gets login.
-     *
-     * @return the login
-     */
-    public String getLogin() {
-        return login;
+    public void registra() {
+        this.isRegistrato = true;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{id=" + id + ", username='" + username + "', email='" + email + "', nome='" + nome + "', cognome='" + cognome + "'}";
+    }
+
 }
